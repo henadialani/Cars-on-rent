6ad728b6fb940bc982082733995aaa87fde8efa7
henadialani <henadialani@gmail.com>
2020-05-03 00:39:06 -0700

    Updates

---
+++ precache-manifest.321bca972ef4f4a467c41ce35f1db53c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc65eab2e37b89be89f88456f224f1f",
+    "url": "/henadialani/Cars-on-rent/index.html"
+  },
+  {
+    "revision": "4f0883e68866a93846ed",
+    "url": "/henadialani/Cars-on-rent/static/css/main.854fae6f.chunk.css"
+  },
+  {
+    "revision": "9bb923e7175af1e28d40",
+    "url": "/henadialani/Cars-on-rent/static/js/2.0968ac6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/henadialani/Cars-on-rent/static/js/2.0968ac6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0883e68866a93846ed",
+    "url": "/henadialani/Cars-on-rent/static/js/main.28c8f5f4.chunk.js"
+  },
+  {
+    "revision": "cc56a9042f7082f54a64",
+    "url": "/henadialani/Cars-on-rent/static/js/runtime-main.1f743379.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/henadialani/Cars-on-rent/static/media/logo.5d5d9eef.svg"
+  }
+]);
